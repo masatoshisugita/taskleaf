c024904e1d56493d38f00ff52edff8d8f49b84b7
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2019-06-29 15:36:22 +0900

    taskleaf

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    #@tasks = current_user.tasks.all
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv,filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
+    end
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      redirect_to @task, notice:"タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice:"タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice:"タスク「#{task.name}」を削除しました。"
+  end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description,:image)
+  end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,28 @@
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+
+  belongs_to :user
+  has_one_attached :image
+
+  def self.csv_attributes
+    ["name","description","created_at","updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
+    end
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new #Task.newと同じ
+      task.attributes = row.to_hash.slice(*csv_attributes)  #アスタリスクは可変長引数
+      task.save!
+    end
+  end
+end
